08bcf5640bb50301462e55fc9b03704a33fd5a61
vaibhav-rbs <vaibhav.rbs@gmail.com>
2016-03-01 08:42:39 -0800

    till section 3

--- terminal_blog/app.py
+++ terminal_blog/app.py
@@ -1,16 +1,9 @@
+import menu
 from database import Database
-from models.post import Post
-from models.blog import Blog
+
 
 Database.initialize()
 
-blog = Blog(author="vaibhav", title="vaibhav is best in the world", description="about my success")
-
-blog.new_post()
-
-blog.save_to_mongo()
-
-from_database = Blog.get_from_mongo(blog.id)
-
-print(blog.get_post())
+menu = menu.Menu()
+menu.run_menu()
 
--- terminal_blog/menu.py
+++ terminal_blog/menu.py
@@ -4,34 +4,49 @@ from models.blog import Blog
 
 class Menu(object):
     def __init__(self):
-        self.user = input("Enter your author name")
+        self.user = input("Enter your author name: ")
         self.user_blog = None
-        if self._user_has_an_account():
-            print("Welcome back {}".format(self.user()))
-
+        if self._user_has_account():
+            print("Welcome back {}".format(self.user))
         else:
-            self._prompt_for_new_user_account()
+            self._prompt_user_for_account()
 
     def _user_has_account(self):
-        blog = Database.find_one('blogs', {'author': self.user}) is not None
+        blog = Database.find_one('blog', {'author': self.user})
         if blog is not None:
-            self.user_blog = blog
+            self.user_blog = Blog.get_from_mongo(blog['id'])
             return True
         else:
             return False
 
     def _prompt_user_for_account(self):
-        title = input("Enter blog title")
-        description = input("Enter blog description:")
+        title = input("Enter blog title: ")
+        description = input("Enter blog description: ")
         blog = Blog(self.user, title, description)
         blog.save_to_mongo()
         self.user_blog = blog
 
     def run_menu(self):
-        read_or_write = input("Do you want to (R)ead or (W)rite blogs")
+        read_or_write = input("Do you want to (R)ead or (W)rite blogs: ")
         if read_or_write == "R":
-            pass
+            self._list_blogs()
+            self._view_blog()
         elif read_or_write == "W":
-            pass
+            self.user_blog.new_post()
         else:
             print("Thankyou for blogging!")
+
+    def _list_blogs(self):
+        blogs = Database.find(collection='blog', query={})
+
+        for blog in blogs:
+            print('ID: {}, Title: {}, Author: {}'.format(blog['id'], blog['title'], blog['author']))
+
+    def _view_blog(self):
+        blog_to_see = input("Enter the ID of the blog you would like to read: ")
+        blog = Blog.get_from_mongo(blog_to_see)
+        posts = blog.get_post()
+        for post in posts:
+            print("Date: {}, title: {} \n\n {}".format(
+                post['created_date'], post['title'], post['content']
+            ))
